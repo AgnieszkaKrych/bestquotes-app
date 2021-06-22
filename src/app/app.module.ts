19a485ae5f4f738a72c337c87656c18078411fe7
AgnieszkaKrych <agnieszka.p.krych@gmail.com>
2021-06-22 18:27:48 +0200

    implementacja,input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RankingComponent } from './ranking/ranking.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, RankingComponent, NavbarComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/ranking/ranking.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Quotation } from '../models/quotation';
+
+@Component({
+  selector: 'app-ranking',
+  templateUrl: './ranking.component.html',
+  styleUrls: ['./ranking.component.css']
+})
+export class RankingComponent {
+
+  constructor() { }
+  
+  @Input()
+  quotes: Quotation[];
+
+  @Input()
+  title: string;
+  
+
+}
